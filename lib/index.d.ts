6ba6c23d44a7ee7ef1e2232c9749c1204f7badb9
mahyargdz <mahyar.goudarzi@outlook.com>
2024-07-08 18:13:14 +0330

    update: package.json

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { definitions } from "./types/options.types";
-declare class Swagger {
-    initialOption: definitions;
-    constructor(options: definitions);
-    init(): void;
-    set(): void;
-}
-export default Swagger;
--- lib/types/options.types.d.ts
+++
@@ -1,9 +0,0 @@
-type definitions = {
-    openapi: string;
-    title: string;
-    version: string;
-    description: string;
-    host: string;
-    path: string;
-};
-export { definitions };
